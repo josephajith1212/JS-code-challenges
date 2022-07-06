6fc96486374efa56beb33dd5b871d1b375de490b
josephajith1212 <josephajith1212@gmail.com>
2022-07-05 21:59:33 -0400

    Binary search solved

--- Extras/FindIndex-BinarySearch.js
+++ Extras/FindIndex-BinarySearch.js
@@ -3,20 +3,16 @@
 var search = function(nums, target) {
     if (nums[0] === target) return 0
     if (nums[nums.length-1] === target) return nums.length-1
-    if (nums[0] > target || nums[nums.length-1] < target) return -1
-    if (nums.length < 2) return -1
-    var i = Math.floor(nums.length/2)
-    while (nums[i] !== target){
-        if (nums[i] > target) i = search(nums.slice(0,i),target)
-        if (nums[i] < target) {
-            var temp = search(nums.slice(i,nums.length-1),target)
-            (temp===-1)? i=-1 : i+=temp
-            // i += search(nums.slice(i,nums.length-1),target)
-        }
-        break
+    
+    let leftIndex = 0, rightIndex = nums.length-1
+    while (leftIndex <= rightIndex && rightIndex-leftIndex > 1){
+        let midIndex = Math.floor((rightIndex+leftIndex)/2)
+        if (nums[midIndex] === target) return midIndex
+        else if (nums[midIndex] > target) rightIndex = midIndex
+        else leftIndex = midIndex
     }
-    return i
+    return -1
 };
 
-// console.log(search([-1,0,3,5,9,12,13,14,15,20,21,22,24,25,31,32,33,34],4))
+// console.log(search([-1,0,3,5,9,12,13,14,15,20,21,22,24,25,31,32,33,34],14))
 console.log(search([-1,0,5],2))
